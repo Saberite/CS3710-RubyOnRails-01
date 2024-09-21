2c926b9cfbe80105ad7d0245a9f5767d855351c4
Bryan Thao <bthao635@gmail.com>
2024-09-21 20:20:57 +0000

    db testing

--- db/migrate/20240919234412_create_students.rb
+++ db/migrate/20240919234412_create_students.rb
@@ -1,5 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[7.1]
   def change
+    #Student Table(User Story 1)
     create_table :students do |t|
       t.string :name
       t.string :school_email
@@ -8,6 +9,13 @@ class CreateStudents < ActiveRecord::Migration[7.1]
       t.date :graduation_date
 
       t.timestamps
+      
     end
+    #Checking Presence for all fields
+    add_index :students, :name,Presence:true
+    add_index :students, :school_email,Presence:true, unique:true
+    add_index :students, :major,Presence:true
+    add_index :students, :minor,Presence:true
+    add_index :students, :graduation_date,Presence:true
   end
 end
--- db/migrate/20240920012705_add_index_to_students_school_email.rb
+++ db/migrate/20240920012705_add_index_to_students_school_email.rb
@@ -1,6 +1,6 @@
 class AddIndexToStudentsSchoolEmail < ActiveRecord::Migration[7.1]
   def change
-    #Checking for Students to make school email unique
+    #Checking for Students to make school email unique(User story 2)
     add_index :students, :school_email,unique:true
   end
 end
