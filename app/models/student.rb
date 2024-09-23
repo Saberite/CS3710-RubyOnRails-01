bc97ed2acf905ae3d6676ab133bcd78ee32ca9f1
Bryan Thao <bthao635@gmail.com>
2024-09-23 21:19:48 +0000

    Active Storage added

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
+    #Command if no image is uploaded for profile picture
+    after_commit :add_default_profile_picture, on: %i[create update]
+
     #User Story #1, each field must be filled
     #Presence = true allows for condiontion to be filied
     validates :first_name, presence:true
@@ -14,8 +17,25 @@ class Student < ApplicationRecord
     
     #Documentation at active record validations on guides/rubyonrails.org
     #Format is used to make sure school_email has '@msudenver.edu'
-    validates :school_email, format: { with: /\A[a-zA-Z]+@msudenver.deu\z/,
-    message: "must be a vaild email" }
+    validates :school_email, format: { with: /\A[a-zA-Z0-9]+@msudenver.edu\z/,
+    message: "must be a vaild MSU email" }
 
+    #Active Storage Attachment Associations
+    has_one_attached :image
 
+    #Default Profile Picture set if not uploaded
+    private
+    def add_default_profile_picture
+        unless image.attached?
+            image.attach(
+                io: File.open(
+                    Rails.root.join(
+                        'app', 'assets', 'images', 'Default_Profile_picture.jpg'
+                    )
+                ), 
+                filename: 'Default_Profile_picture.jpg',
+                content_type: 'image/jpg'
+            )
+        end
+    end
 end
