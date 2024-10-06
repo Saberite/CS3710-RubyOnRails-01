514c9e46dbb6e7b21b2a8e65295cd0d7075859e8
Bryan Thao <bthao635@gmail.com>
2024-10-06 00:06:03 +0000

    Added Dropdown for Major in _form.html and validation for Major in Students.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,15 +2,21 @@ class Student < ApplicationRecord
     #Command if no image is uploaded for profile picture
     after_commit :add_default_profile_picture, on: %i[create update]
 
-    #User Story #1, each field must be filled
+    
     #Presence = true allows for condiontion to be filied
     validates :first_name, presence:true
     validates :last_name, presence:true
-    validates :major, presence:true
     validates :graduation_date, presence:true
     #Minor does not need to be filled
 
-    #User Story #2, must be a unique email
+    #Sprint #2 VALID_MAJORS
+    #Purpose is only accept major listed below
+    VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS", "Computer Science BS", "Cybersecurity Major", "Data Science and Machine Learning Major"]
+    
+    #Major includes validations for text in VALID_MAJORS
+    validates :major, presence:true
+
+    
     #Uniqueness: true allows for condiontion to be realized
     validates :school_email, presence: true 
     validates :school_email, uniqueness:true
